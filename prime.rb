bb397b9904ea6a964e947a44737de9c74db51dac
Marc Steele <marcsteele2014@gmail.com>
2020-10-11 12:43:48 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,10 @@
-# Add  code here!
+def prime?(integer)
+  divisors = (2...integer).to_a
+  divisors.each do |num|
+    if integer % num == 0
+      return false
+    else
+      return true
+    end
+  end
+end
